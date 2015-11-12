3a68b140c1fa3baea5d35f378a7c0c8021712872
Oliver Wang <oliverwangmusic@gmail.com>
2015-11-11 16:34:31 -0800

    implemented setDirection and getDirection

--- Server/Snake.js
+++ Server/Snake.js
@@ -1,5 +1,28 @@
-Snake = function (positionX, positionY, InitialDirection) {
-  this.positionX = positionX;
-  this.positionY = positionY;
-  this.InitialDirection = InitialDirection;
+Snake = function (headX, headY, InitialDirection) {
+  this.headX = headX;
+  this.headY  = headY;
+  this.currDir = InitialDirection;
+  this.length = 3;
+  this.body = [ [headX,headY] ]
+  Snake.prototype.init();
 }
+
+Snake.prototype.init = function() {
+  var tailDir = {
+    up: [0, 1],
+    down: []
+  }
+  if (this.currDir === 'up') {
+    this.body.push( [this.headX, this.headY + 1] )
+  }
+};
+
+Snake.prototype.setDirection = function ( direction ) {
+  this.currDir = direction;
+};
+
+Snake.prototype.getDirection = function () {
+  return this.currDir;
+};
+
+Snake.prototype

3c7e20c09f007d7b72a01b42bf94cb4f9f9e94c9
AVickory <andrew.vickory@gmail.com>
2015-11-12 19:05:19 -0800

    finished button logic.

--- client/scripts/buttonInit.js
+++ client/scripts/buttonInit.js
@@ -1,20 +1,35 @@
 (function (app) {
   'use strict';
+  window.socketOpen = false
 
   var readyButton = document.getElementById('readyButton');
   readyButton.pressed = false;
 
-  var applyLoadingStyles = function () {
-    // change button css to load state class
-    readyButton.className = 'load';
-  };
-  
+  var setButtonStyle = function () {
+    if(readyButton.pressed) {
+      readyButton.className = 'load';
+    } else if (window.myBoard.gameStart) {
+      readyButton.className = 'game-on';
+    } else {
+      readyButton.className = 'pre-click';
+    }
+  }
   readyButton.addEventListener('click', function () {
-    console.log('button clicked');
-    app.socket = io('http://localhost:8080');
-    applyListeners();
+    if(!this.pressed) {
+      if(!socketOpen) {
+        var socket = io('http://localhost:8080');
+        window.socketOpen = true
+        socket.on('update', function (gameData) {
+          window.myBoard.updateBoard(gameData);
+          window.myBoard.gameStart = true;
+          setButtonStyle();
+          //We need to add listeners here for game end, starting a new game(say the second or third) and countdown
+        })
+      }
+      this.pressed = true;
+      setButtonStyle();
+    }
   });
 
-  // setup up another listener for game start that makes the button fade out, or something.
 
 }(window.app));

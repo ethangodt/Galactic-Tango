c1c73762cc181a8e36656f4686827ea177cb0adf
AVickory <andrew.vickory@gmail.com>
2015-11-16 14:22:12 -0800

    documented buttoninit

--- client/scripts/buttonInit.js
+++ client/scripts/buttonInit.js
@@ -1,91 +1,94 @@
 (function (app) {
   'use strict';
-  app.gameStart = false;
-  app.userId;
-  var gameOver = document.getElementById('gameOver');
-  console.log(gameOver);
-
+  readyButton.pressed = false; //keeps track of whether or not the button is clickable.
+  app.gameStart = false; // keeps track of whether a game is currently in progress
+  app.userId; // the index of the user in the snakes array
+  var gameOver = document.getElementById('gameOver'); //the lose/win message div
   var readyButton = document.getElementById('readyButton');
-  readyButton.pressed = false;
 
+  //sets the class of the ready button based on game state.  only the init style has been implemented
   var setButtonStyle = function () {
     if(readyButton.pressed) {
       readyButton.className = 'load';
+      readyButton.textContent = 'Waiting...'
     }
     if (app.gameStart) {
       readyButton.className = 'game-on';
+      readyButton.textContent = 'NOM NOM NOM'
     } else {
       readyButton.className = 'init';
+      readyButton.textContent = 'Ready';
     }
   };
 
+  //checks if the button has been pressed or a game has started.  if it hasn't then it opens the socket and
+  //resets the board, gets rid of the win/lose message, sets the button text to 'Waiting...'
   readyButton.addEventListener('click', function () {
-
     if(!this.pressed && !app.gameStart) {
       app.board.updateBoard();
       openSocket();
       this.pressed = true;
       gameOver.style.display = 'none';
-      readyButton.textContent = 'Waiting...'
+      setButtonStyle()
     }
-
   });
 
+  //sets and displays the lose message
   var iLost = function () {
-    console.log('LOSER');
     gameOver.textContent = 'ALL HAIL ' + app.board.snakeColors[app.userId] + ', KING OF THE LOSERS!';
     gameOver.style.display = 'inline-block';
   };
+  //sets and displays the win message
   var iWon = function () {
-    console.log('WINNER')
     gameOver.textContent = '"' + app.board.snakeColors[app.userId] + '" IS VICTORIOUS! NOMNOMNOM!'
     gameOver.style.display = 'inline-block';
   };
 
+  //sets the border color or the board based on the userId
   var setBorderColor = function () {
     var color = app.board.snakeColors[app.userId];
     var board = document.getElementsByClassName('boardContainer')[0];
     board.style.borderColor = color;
   };
 
+  //if a socket hasn't been opened, it opens one and attaches listeners, otherwise it emits a 'ready' signal
+  //to the server to be put in a new room.
   var openSocket = function () {
     if(!app.socket) {
       app.socket = io();
-
+      //updates the game board when the server tells it to
       app.socket.on('update', function (gameData) {
         app.board.updateBoard(gameData);
-        //might want to move this to the game start listener when we have that.
-        //We need to add listeners here for game end, starting a new game(say the second or third) and countdown
-        //*put them here.
       });
 
+      //resets button and state variables on 'game over' signal, displays win/lose message.  It leaves the board
+      //as it was at the end of the game.
       app.socket.on('game over', function (winner) {
-        console.log(winner)
         readyButton.pressed = false;
         app.gameStart = false;
+
         setButtonStyle();
-        readyButton.textContent = 'Ready';
         if(app.userId === winner) {
           iWon();
         } else {
           iLost();
         }
-        // app.socket.io.disconnect();
-
       });
 
+      //on 'gameStart' signal starts a countdown in the ready button.  removes the win/lose message, changes the 
+      //border color, sets gameStart to true.  Sets the button to gameMode.
       app.socket.on('gameStart', function (userId) {
         gameOver.style.display = 'none';
         app.userId = userId;
-        setBorderColor();
-        var counter = 4;
         app.gameStart = true;
+        setBorderColor();
         setButtonStyle();
+
+        var counter = 4;
         readyButton.textContent = --counter;
         var timer = setInterval(function () {
           readyButton.textContent = --counter;
           if(counter === 0) {
-            readyButton.textContent = 'NOM NOM NOM'
             clearInterval(timer);
           }
         }, 1000);

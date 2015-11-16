ebb0248c0bc6c2d379ca906fd05dcb16c15c374e
Oliver Wang <oliverwangmusic@gmail.com>
2015-11-16 15:33:56 -0800

    ignored some lines for jshint

--- client/scripts/socketLogic.js
+++ client/scripts/socketLogic.js
@@ -1,4 +1,6 @@
+/* jshint ignore:start */
+//listen for board updates through the current board and call the updateBoard function
 function applyListeners () {
-  //listen for board updates through the current board and call the updateBoard function
   app.socket.on('update', app.board.updateBoard.bind(app.board));
 }
+/* jshint ignore:end */
